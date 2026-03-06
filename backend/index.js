fb3288fb898d3554d9b5a0af79e7963d924baf8f
juancardaw <juancarlosgym15@gmail.com>
2026-03-06 04:44:37 +0100

    Implementacion del cors en Index.js del backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors'); 
 const { conectarDB, sequelize } = require('./config/db');
 require('dotenv').config();
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: 'http://localhost:5173', // La URL del Frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos de interaccion
+    allowedHeaders: ['Content-Type', 'Authorization'] // Cabeceras permitidas 
+}));
 
 app.use(express.json());
 conectarDB();
